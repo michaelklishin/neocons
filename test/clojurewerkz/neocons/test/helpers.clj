c793790950aa12f706a05674ce960da716f59ca8
Michael S. Klishin <michael@novemberain.com>
2011-11-30 18:18:28 +0400

    Separate test for neocons.helpers/extract-id

---
+++ test/clojurewerkz/neocons/test/helpers.clj
@@ -0,0 +1,11 @@
+(ns clojurewerkz.neocons.test.helpers
+  (:use [clojure.test]
+        [clojurewerkz.neocons.rest.helpers]))
+
+(deftest test-id-extraction
+  (is (= 1 (extract-id "http://localhost:7474/db/data/node/1")))
+  (is (= 10 (extract-id"http://localhost:7474/db/data/node/10")))
+  (is (= 100 (extract-id"http://localhost:7474/db/data/node/100")))
+  (is (= 1000 (extract-id"http://localhost:7474/db/data/node/1000")))
+  (doseq [id (range 1 10000)]
+    (is (= id (extract-id (str "http://localhost:7474/db/data/node/" id))))))
