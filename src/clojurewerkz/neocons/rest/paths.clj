52c527ea4b7fa0c1183ca50bd357f26c79232f37
Michael S. Klishin <michael@novemberain.com>
2011-12-04 17:31:00 +0400

    Implement shortest path(s) algorithms support

--- src/clojurewerkz/neocons/rest/paths.clj
+++ src/clojurewerkz/neocons/rest/paths.clj
@@ -16,6 +16,21 @@
   [^Neo4JEndpoint endpoint ^long id]
   (str (:node-uri endpoint) "/" id "/traverse/path"))
 
+(defn paths-location-for
+  [^Neo4JEndpoint endpoint ^long id]
+  (str (:node-uri endpoint) "/" id "/paths"))
+
+(defn path-location-for
+  [^Neo4JEndpoint endpoint ^long id]
+  (str (:node-uri endpoint) "/" id "/path"))
+
+(defn node-location-for
+  [^Neo4JEndpoint endpoint ^long id]
+  (str (:node-uri endpoint) "/" id))
+
+(defn rel-location-for
+  [^Neo4JEndpoint endpoint ^long id]
+  (str (:relationships-uri endpoint) "/" id))
 
 
 ;;
@@ -40,3 +55,70 @@
            xs (json/read-json body true)]
        (map (fn [doc]
               (instantiate-path-from doc)) xs))))
+
+
+(defn all-shortest-between
+  ([from to & { :keys [order relationships uniqueness prune-evaluator return-filter max-depth] :or {
+                                                                                                    order         "breadth_first"
+                                                                                                    uniqueness    "none"
+                                                                                                    prune-evaluator { :language "builtin" :name "none" }
+                                                                                                    return-filter   { :language "builtin" :name "all"  }
+                                                                                                    } }]
+     (let [request-body {
+                         :to            (node-location-for rest/*endpoint* to)
+                         :relationships relationships
+                         :max_depth     max-depth
+                         :algorithm     "shortestPath"
+                         }
+           { :keys [status body] } (rest/POST (paths-location-for rest/*endpoint* from) :body (json/json-str request-body))
+           xs (json/read-json body true)]
+       (map (fn [doc]
+              (instantiate-path-from doc)) xs))))
+
+
+(defn shortest-between
+  ([from to & { :keys [order relationships uniqueness prune-evaluator return-filter max-depth] :or {
+                                                                                                    order         "breadth_first"
+                                                                                                    uniqueness    "none"
+                                                                                                    prune-evaluator { :language "builtin" :name "none" }
+                                                                                                    return-filter   { :language "builtin" :name "all"  }
+                                                                                                    } }]
+     (let [request-body {
+                         :to            (node-location-for rest/*endpoint* to)
+                         :relationships relationships
+                         :max_depth     max-depth
+                         :algorithm     "shortestPath"
+                         }
+           { :keys [status body] } (rest/POST (path-location-for rest/*endpoint* from) :body (json/json-str request-body))
+           payload (json/read-json body true)]
+       (instantiate-path-from payload))))
+
+(defprotocol PathPredicates
+  (included-in?     [node path] "Returns true if path includes given node")
+  (included-in?     [rel  path] "Returns true if path includes given relationship")
+  (node-in?         [node path] "Returns true if path includes given node")
+  (relationship-in? [rel  path] "Returns true if path includes given relationship"))
+
+
+(extend-protocol PathPredicates
+  Long
+  (node-in? [^long node ^Path path]
+    (let [uri (node-location-for rest/*endpoint* node)]
+      (some (fn [u]
+              (= uri u)) (:nodes path))))
+  (relationship-in? [^long rel ^Path path]
+    (let [uri (rel-location-for rest/*endpoint* rel)]
+      (some (fn [u]
+              (= uri u)) (:relationships path))))
+
+  Node
+  (included-in? [^Node node ^Path path]
+    (node-in? (:id node) path))
+  (node-in?     [^Node node ^Path path]
+    (node-in? (:id node) path))
+
+  Relationship
+  (included-in? [^Relationship rel ^Path path]
+    (relationship-in? (:id rel) path))
+  (relationship-in? [^Relationship rel ^Path path]
+    (relationship-in? (:id rel) path)))
--- test/clojurewerkz/neocons/test/rest.clj
+++ test/clojurewerkz/neocons/test/rest.clj
@@ -332,6 +332,10 @@
                   (= id (:id puma))) ids)))))
 
 
+;;
+;; Traversal
+;;
+
 (deftest test-traversing-nodes-using-all-return-filter-all-relationships-and-no-pagination
   (let [john (nodes/create { :name "John" })
         adam (nodes/create { :name "Alan" })
@@ -415,3 +419,67 @@
       (is (= (:location-uri adam) (:end   path2)))
       (is (= (:location-uri john) (:start path3)))
       (is (= (:location-uri pete) (:end   path3))))))
+
+
+;;
+;; Shortest path algorithm
+;;
+
+(deftest test-shortest-path-algorithm-1
+  (let [john (nodes/create { :name "John" :age 28 :location "New York City, NY" })
+        liz  (nodes/create { :name "Liz"  :age 27 :location "Buffalo, NY" })
+        beth (nodes/create { :name "Elizabeth" :age 30 :location "Chicago, IL" })
+        bern (nodes/create { :name "Bernard"   :age 33 :location "Zürich" })
+        gael (nodes/create { :name "Gaël"      :age 31 :location "Montpellier" })
+        rel1 (relationships/create john liz  :knows)
+        rel2 (relationships/create liz  beth :knows)
+        rel3 (relationships/create liz  bern :knows)
+        rel4 (relationships/create bern gael :knows)
+        rel5 (relationships/create gael beth :knows)
+        rel6 (relationships/create beth gael :knows)
+        rel7 (relationships/create john gael :knows)
+        rt   { :type "knows" :direction "out" }
+        xs1   (paths/all-shortest-between (:id john) (:id liz)  :relationships [rt] :max-depth 1)
+        path1 (first xs1)
+        xs2 (paths/all-shortest-between (:id john) (:id beth) :relationships [rt] :max-depth 1)
+        xs3 (paths/all-shortest-between (:id john) (:id beth) :relationships [rt] :max-depth 2)
+        path3 (first xs3)
+        xs4 (paths/all-shortest-between (:id john) (:id beth) :relationships [rt] :max-depth 3)
+        path4 (first xs4)
+        xs5 (paths/all-shortest-between (:id john) (:id beth) :relationships [rt] :max-depth 7)
+        path5 (first xs5)
+        path6 (last  xs5)
+        path7 (paths/shortest-between (:id john) (:id beth) :relationships [rt] :max-depth 7)]
+    (is (empty? xs2))
+    (is (= 1 (count xs1)))
+    (is (= 2 (count xs3)))
+    (is (= 2 (count xs4)))
+    (is (= 2 (count xs5)))
+    (is (= (:start path1) (:location-uri john)))
+    (is (= (:end   path1) (:location-uri liz)))
+    (is (= 2 (:length path3)))
+    (is (= (:start path3) (:location-uri john)))
+    (is (= (:end   path3) (:location-uri beth)))
+    (is (= 2 (:length path4)))
+    (is (= (:start path4) (:location-uri john)))
+    (is (= (:end   path4) (:location-uri beth)))
+    (is (= 2 (:length path5)))
+    (is (= (:start path5) (:location-uri john)))
+    (is (= (:end   path5) (:location-uri beth)))
+    (is (= 2 (:length path6)))
+    (is (= (:start path6) (:location-uri john)))
+    (is (= (:end   path6) (:location-uri beth)))
+    (is (= 2 (:length path7)))
+    (is (= (:start path7) (:location-uri john)))
+    (is (= (:end   path7) (:location-uri beth)))
+    (is (paths/node-in? (:id john) path7))
+    (is (not (paths/node-in? (:id bern) path7)))
+    (is (paths/node-in? john path7))
+    (is (not (paths/node-in? bern path7)))
+    (is (paths/included-in? john path7))
+    (is (paths/included-in? rel1 path7))
+    (is (paths/relationship-in? (:id rel1) path7))
+    (is (paths/relationship-in? rel1 path7))
+    (is (not (paths/included-in? rel4 path7)))
+    (is (not (paths/relationship-in? (:id rel4) path7)))
+    (is (not (paths/relationship-in? rel4 path7)))))
