703ae72f8987d64f0add56419e71ca06eed2c180
Michael S. Klishin <michael@novemberain.com>
2012-02-25 18:48:40 +0400

    1.0.0-beta1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/neocons "1.0.0-SNAPSHOT"
+(defproject clojurewerkz/neocons "1.0.0-beta1"
   :description "Neocons is an experimental idiomatic Clojure client for the Neo4J REST API"
   :license { :name "Eclipse Public License" }
   :repositories {
