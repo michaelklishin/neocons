ccb3274f6ee716c6274f8e8e530aacf526c48cfd
Michael S. Klishin <michael@novemberain.com>
2012-03-10 21:45:43 +0400

    Migrate to Leiningen 2

--- project.clj
+++ project.clj
@@ -1,28 +1,30 @@
 (defproject clojurewerkz/neocons "1.0.0-SNAPSHOT"
-  :description "Neocons is an experimental idiomatic Clojure client for the Neo4J REST API"
-  :license { :name "Eclipse Public License" }
-  :repositories {
-                 "clojure-releases" "http://build.clojure.org/releases"
-                 "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
-                             :snapshots false
-                             :releases {:checksum :fail :update :always}
-                             }
-                 }
-  :dependencies [[org.clojure/clojure   "1.3.0"]
+  :license {:name "Eclipse Public License"}
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.json "0.1.2"]
-                 [clj-http              "0.3.3"]]
-  :multi-deps {
-               "1.4" [[org.clojure/clojure "1.4.0-beta4"]]
-               :all [[org.clojure/data.json "0.1.2"]
-                     [clj-http              "0.3.3"]]
-               }
-  :source-path        "src/clojure"
-  :java-source-path   "src/java"
-  :dev-resources-path "test/resources"
+                 [clj-http "0.3.3"]]
+  :test-selectors {:default (fn [v] (not (:time-consuming v))),
+                   :time-consuming (fn [v] (:time-consuming v)),
+                   :focus (fn [v] (:focus v)),
+                   :indexing (fn [v] (:indexing v)),
+                   :cypher (fn [v] (:cypher v)),
+                   :all (fn [_] true)}
+  :source-paths ["src/clojure"]
+  :profiles {:all
+             {:dependencies
+              [[org.clojure/data.json "0.1.2"] [clj-http "0.3.3"]]},
+             :1.4
+             {:dependencies [[org.clojure/clojure "1.4.0-beta4"]]}}
+  :test-resources-path "/Users/antares/Development/ClojureWerkz/neocons.git/test/resources"
+  :repositories {"clojure-releases"
+                 "http://build.clojure.org/releases",
+                 "sonatype"
+                 {:url
+                  "http://oss.sonatype.org/content/repositories/releases",
+                  :snapshots false,
+                  :releases {:checksum :fail, :update :always}}}
+  :java-source-paths ["src/java"]
+  :min-lein-version "2.0.0"
+  :dev-resources-path "/Users/antares/Development/ClojureWerkz/neocons.git/test/resources"
   :warn-on-reflection true
-  :test-selectors   {:default        (fn [v] (not (:time-consuming v)))
-                     :time-consuming (fn [v] (:time-consuming v))
-                     :focus          (fn [v] (:focus v))
-                     :indexing       (fn [v] (:indexing v))
-                     :cypher         (fn [v] (:cypher v))
-                     :all            (fn [_] true)})
+  :description "Neocons is an experimental idiomatic Clojure client for the Neo4J REST API")
