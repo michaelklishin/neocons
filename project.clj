139a4df4af2dfe0561ed9f0a43f880d44cb34596
Michael S. Klishin <michael@novemberain.com>
2012-04-27 00:02:15 +0400

    Test against 1.5-master-SNAPSHOT on travis-ci.org

--- project.clj
+++ project.clj
@@ -17,8 +17,7 @@
   :source-paths ["src/clojure"]
   :profiles       {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
                    :1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}}
-  :aliases        {"ci"  ["with-profile" "dev:dev,1.4"]
-                   "all" ["with-profile" "dev:dev,1.4:dev,1.5"]}
+  :aliases        {"all" ["with-profile" "dev:dev,1.4:dev,1.5"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
