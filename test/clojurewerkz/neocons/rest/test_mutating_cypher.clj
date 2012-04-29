71476e633438ba9e675f648d4fbc4ff99b61e3fa
Michael S. Klishin <michael@novemberain.com>
2012-04-29 23:36:32 +0400

    Several more test cases for mutating Cypher in Neo4J 1.8 (snapshot)

--- test/clojurewerkz/neocons/rest/test_mutating_cypher.clj
+++ test/clojurewerkz/neocons/rest/test_mutating_cypher.clj
@@ -1,7 +1,8 @@
 (ns clojurewerkz.neocons.rest.test-mutating-cypher
   (:require [clojurewerkz.neocons.rest               :as neorest]
             [clojurewerkz.neocons.rest.nodes         :as nodes]
-            [clojurewerkz.neocons.rest.relationships :as relationships]
+            [clojurewerkz.neocons.rest.paths         :as paths]
+            [clojurewerkz.neocons.rest.relationships :as rel]
             clojurewerkz.neocons.rest.records
             [clojurewerkz.neocons.rest.cypher        :as cy])
   (:use clojure.test)
@@ -25,12 +26,32 @@
 ;; Mutating Cypher queries
 ;;
 
+(deftest ^{:cypher true :edge-features true} test-creating-a-node-via-mutating-cypher
+  (testing "without node properties"
+    (let [response (cy/tquery "CREATE n RETURN n")]
+      (is (empty? (get-in (first response) ["n" :data])))))
+  (testing "with node properties"
+    (let [response (cy/tquery "CREATE n = {name: 'Neocons', type: 'client', language: 'Clojure'} RETURN n")]
+      (is (= {:name "Neocons" :language "Clojure" :type "client"} (get-in (first response) ["n" :data])))))
+  (testing "with node properties passed as a map"
+    (let [props    {:name "Neocons" :language "Clojure" :type "client"}
+          response (cy/tquery "CREATE n = {props} RETURN n" {:props props})]
+      (is (= props (get-in (first response) ["n" :data]))))))
+
+(deftest ^{:cypher true :edge-features true} test-creating-a-relationship-between-nodes-via-mutating-cypher
+  (let [n1    (nodes/create)
+        n2    (nodes/create)
+        [{r "r"}] (cy/tquery "START n1 = node({id1}), n2 = node({id2}) CREATE n1-[r:knows]->n2 RETURN r" {:id1 (:id n1)
+                                                                                                        :id2 (:id n2)})]
+    (is (rel/starts-with? r (:id n1)))
+    (is (rel/ends-with? r (:id n2)))))
+
 (deftest ^{:cypher true :edge-features true} test-purging-a-node-via-mutating-cypher-case1
   (let [john (nodes/create {:name "John" :age 28 :location "New York City, NY"})
         beth (nodes/create {:name "Elizabeth" :age 30 :location "Chicago, IL"})
         gael (nodes/create {:name "Gaël"      :age 31 :location "Montpellier"})
-        rel1 (relationships/create john beth :knows)
-        rel2 (relationships/create john gael :knows)]
+        rel1 (rel/create john beth :knows)
+        rel2 (rel/create john gael :knows)]
     (is (thrown? Exception
                  (nodes/delete (:id john))))
     (is (cy/empty? (cy/query "START n = node({sid}) MATCH n-[r]-() DELETE n, r" {:sid (:id john)})))
