michaelklishin/neocons

commit 8eae2756a8c6138e96bb13eec4ca7ba8acf2ec56

Refactor record instantiation to use map->RecordName

src/clojure/clojurewerkz/neocons/rest/records.clj
@@ -88,21 +88,21 @@
 (defn instantiate-node-from
   ([payload]
      (let [id (extract-id (:self payload))]
-       (Node. id (:self payload) (:data payload) (:all_relationships payload) (:create_relationship payload))))
+       (map->Node (merge {:id id} payload))))
   ([payload ^long id]
-     (Node. id (:self payload) (:data payload) (:all_relationships payload) (:create_relationship payload))))
+     (map->Node (merge {:id id} payload))))
 
 (defn instantiate-rel-from
   ([payload]
      (let [id (extract-id (:self payload))]
-       (Relationship. id (:self payload) (:start payload) (:end payload) (:type payload) (:data payload))))
+       (map->Relationship (merge {:id id} payload))))
   ([payload ^long id]
-     (Relationship. id (:self payload) (:start payload) (:end payload) (:type payload) (:data payload))))
+     (map->Relationship (merge {:id id} payload))))
 
 (defn instantiate-path-from
-  ([payload]
-     (Path. (:start payload) (:end payload) (:length payload) (:nodes payload) (:relationships payload))))
+  [payload]
+  (map->Path payload))
 
 (defn instantiate-cypher-query-response-from
   [payload]
-  (CypherQueryResponse. (:data payload) (:columns payload)))
+  (map->CypherQueryResponse payload))

test/clojurewerkz/neocons/rest/test_cypher.clj
@@ -1,7 +1,8 @@
 (ns clojurewerkz.neocons.rest.test-cypher
   (:require [clojurewerkz.neocons.rest               :as neorest]
             [clojurewerkz.neocons.rest.nodes         :as nodes]
-            [clojurewerkz.neocons.rest.relationships :as relationships]
+            [clojurewerkz.neocons.rest.relationships :as rel]
+            [clojurewerkz.neocons.rest.paths         :as paths]
             [clojurewerkz.neocons.rest.cypher        :as cy])
   (:use clojure.test
         [clojure.set :only [subset?]]
@@ -16,16 +17,16 @@
 ;;
 
 (deftest ^{:cypher true} test-cypher-query-example1
-  (let [john  (nodes/create { :name "John" })
-        sarah (nodes/create { :name "Sarah" })
-        joe   (nodes/create { :name "Joe" })
-        maria (nodes/create { :name "Maria" })
-        steve (nodes/create { :name "Steve" })
-        rel1  (relationships/create john sarah :friend)
-        rel2  (relationships/create john joe :friend)
-        rel3  (relationships/create sarah maria :friend)
-        rel4  (relationships/create joe steve :friend)
-        { :keys [data columns] } (cy/query "START john=node({sid}) MATCH john-[:friend]->()-[:friend]->fof RETURN john, fof" { :sid (:id john) })
+  (let [john  (nodes/create {:name "John"})
+        sarah (nodes/create {:name "Sarah"})
+        joe   (nodes/create {:name "Joe"})
+        maria (nodes/create {:name "Maria"})
+        steve (nodes/create {:name "Steve"})
+        rel1  (rel/create john sarah :friend)
+        rel2  (rel/create john joe :friend)
+        rel3  (rel/create sarah maria :friend)
+        rel4  (rel/create joe steve :friend)
+        {:keys [data columns]} (cy/query "START john=node({sid}) MATCH john-[:friend]->()-[:friend]->fof RETURN john, fof" {:sid (:id john)})
         row1  (map instantiate-node-from (first  data))
         row2  (map instantiate-node-from (second data))]
     (is (= 2 (count data)))
@@ -41,24 +42,20 @@
 
 
 (deftest ^{:cypher true} test-cypher-query-example2
-  (let [john  (nodes/create { :name "John" })
-        sarah (nodes/create { :name "Sarah" })
-        rel1  (relationships/create john sarah :friend)
-        { :keys [data columns] } (cy/query "START x = node({sid}) MATCH path = (x--friend) RETURN path, friend.name" { :sid (:id john) })
-        row1  (map instantiate-path-from (first data))
-        path1 (first row1)]
-    (is (= 1 (count data)))
-    (is (= ["path" "friend.name"] columns))
-    (is (= 1 (:length path1)))
-    (is (= (:start path1) (:location-uri john)))
-    (is (= (:end   path1) (:location-uri sarah)))
-    (is (= (first (:relationships path1)) (:location-uri rel1)))))
+  (let [john  (nodes/create {:name "John"})
+        sarah (nodes/create {:name "Sarah"})
+        rel1  (rel/create john sarah :friend)
+        [row1] (cy/tquery "START x = node({sid}) MATCH path = (x--friend) RETURN path, friend.name" {:sid (:id john)})
+        path   (instantiate-path-from (get row1 "path"))]
+    (is (paths/included-in? john path))
+    (is (paths/included-in? sarah path))
+    (is (= "Sarah" (get row1 "friend.name")))))
 
 
 (deftest ^{:cypher true} test-cypher-query-example3
   (let [john  (nodes/create { :name "John"  :age 27 })
         sarah (nodes/create { :name "Sarah" :age 28 })
-        rel1  (relationships/create john sarah :friend)
+        rel1  (rel/create john sarah :friend)
         ids   (map :id [john sarah])
         { :keys [data columns] :as response } (cy/query "START x = node({ids}) RETURN x.name, x.age" { :ids ids })]
     (is (= ["John" "Sarah"] (vec (map first data))))
@@ -81,7 +78,7 @@
 (deftest ^{:cypher true} test-cypher-tquery
   (let [john  (nodes/create { :name "John"  :age 27 })
         sarah (nodes/create { :name "Sarah" :age 28 })
-        rel1  (relationships/create john sarah :friend)
+        rel1  (rel/create john sarah :friend)
         ids   (map :id [john sarah])]
     (is (= [{"x.name" "John"  "x.age" 27}
             {"x.name" "Sarah" "x.age" 28}]

commit a9ec71d48416942463bdea8dd5dcff776dc0a908

Make sure relationships/create handles nodes that may not have :create-relationship-uri set
It happens in part because of slightly varying Neo4J REST API responses and in part
due to our own regressions in 8eae2756a8c6138e96bb13eec4ca7ba8acf2ec56

src/clojure/clojurewerkz/neocons/rest/records.clj
@@ -89,16 +89,16 @@
 (defn instantiate-node-from
   ([payload]
      (let [id (extract-id (:self payload))]
-       (map->Node (merge {:id id} payload))))
+       (Node. id (:self payload) (:data payload) (:all_relationships payload) (:create_relationship payload))))
   ([payload ^long id]
-     (map->Node (merge {:id id} payload))))
+     (Node. id (:self payload) (:data payload) (:all_relationships payload) (:create_relationship payload))))
 
 (defn instantiate-rel-from
   ([payload]
      (let [id (extract-id (:self payload))]
-       (map->Relationship (merge {:id id} payload))))
+       (Relationship. id (:self payload) (:start payload) (:end payload) (:type payload) (:data payload))))
   ([payload ^long id]
-     (map->Relationship (merge {:id id} payload))))
+     (Relationship. id (:self payload) (:start payload) (:end payload) (:type payload) (:data payload))))
 
 (defn instantiate-path-from
   [payload]

src/clojure/clojurewerkz/neocons/rest/relationships.clj
@@ -22,6 +22,10 @@
                        "")]
     (str (:node-uri endpoint) "/" (:id node) "/relationships/" (name kind) query-params)))
 
+(defn- create-relationship-location-for
+  [^Neo4JEndpoint endpoint ^Node node]
+  (str (:node-uri endpoint) "/" (:id node) "/relationships/"))
+
 (defn- relationships-for
   [^Node node kind types]
   (let [{ :keys [status headers body] } (rest/GET (relationships-location-for rest/*endpoint* node kind types))
@@ -40,16 +44,21 @@
   ([^Node from ^Node to rel-type]
      (create from to rel-type {}))
   ([^Node from ^Node to rel-type data]
-     (let [{ :keys [status headers body] } (rest/POST (:create-relationship-uri from)
-                                                      :body (json/json-str { :to (:location-uri to) :type rel-type :data data }))
+     ;; these (or ...)s here are necessary because Neo4J REST API returns nodes in different format when fetched via nodes/get
+     ;; and found via index. We have to account for that. MK.
+     (let [{:keys [status headers body]} (rest/POST (or (:create-relationship-uri from)
+                                                        (create-relationship-location-for rest/*endpoint* from))
+                                                    :body (json/json-str {:to (or (:location-uri to)
+                                                                                  (node-location-for rest/*endpoint* (:id to))) :type rel-type :data data}))
            payload  (json/read-json body true)]
        (instantiate-rel-from payload))))
 
 (defn create-many
   "Concurrently creates multiple relations of given type between the *from* node and several provded nodes.
-   All relationships will be of the same time. This function should be used when number of relationships
-   that need to be created is moderately high (dozens and more), otherwise it would be less efficient than
-   using clojure.core/map over the same sequence of nodes"
+   All relationships will be of the same type.
+
+   This function should be used when number of relationships that need to be created is moderately high (dozens and more),
+   otherwise it would be less efficient than using clojure.core/map over the same sequence of nodes"
   ([^Node from xs rel-type]
      (pmap (fn [^Node n]
              (create from n rel-type)) xs))

test/clojurewerkz/neocons/rest/test_integration.clj
@@ -36,11 +36,17 @@
   (nn/create-index "by-url" {:type "exact"})
   (let [homepage  (nn/create {:url "http://clojurewerkz.org/"})
         community (nn/create {:url "http://clojurewerkz.org/articles/community.html"}
-                             {"by-url" [:url "http://clojurewerkz.org/articles/community.html"]})]
-    (nn/add-to-index homepage "by-url" :url "http://clojurewerkz.org/")
+                             {"by-url" [:url "http://clojurewerkz.org/articles/community.html"]})
+        _             (nn/add-to-index homepage "by-url" :url "http://clojurewerkz.org/")
+        homepage-alt  (nn/find-one "by-url" :url "http://clojurewerkz.org/")
+        community-alt (nn/find-one "by-url" :url "http://clojurewerkz.org/articles/community.html")]
     (is (= (:id homepage)
-           (:id (nn/find-one "by-url" :url "http://clojurewerkz.org/"))))
+           (:id homepage-alt)))
     (is (= (:id community)
-           (:id (nn/find-one "by-url" :url "http://clojurewerkz.org/articles/community.html"))))
+           (:id community-alt)))
     (is (nil? (nn/find-one "by-url" :url "http://example99.com")))
-    (nn/delete-index "by-url")))
+    ;; Neo4J REST API returns nodes in different format via index and regular GET. Make sure we handle
+    ;; both cases.
+    (nr/create homepage-alt community-alt :links)
+    (nn/delete-index "by-url")
+    (nn/destroy-many [community-alt homepage-alt])))