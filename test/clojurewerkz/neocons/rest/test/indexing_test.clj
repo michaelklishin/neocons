4020faee12fb363e0f223fe9a258c57d06bfb8ee
Michael S. Klishin <michael@novemberain.com>
2012-08-30 13:50:29 +0400

    POST index configuration correctly, fix fragile non-idempotent tests around indexing. Closes #38
    If you had previously run full suite, you will likely need to rm -rf $NEO4J_ROOT/data/graph.db
    to make sure no indexes conflict with those previously defined w/o configuration.

--- test/clojurewerkz/neocons/rest/test/indexing_test.clj
+++ test/clojurewerkz/neocons/rest/test/indexing_test.clj
@@ -19,23 +19,25 @@
     (rels/create-index name)))
 
 (deftest ^{:indexing true} test-create-a-new-node-index-with-explicit-configuration
-  (let [name "node-index-2"
+  (let [name "test-create-a-new-node-index-with-explicit-configuration-node-index-1"
         conf {:type "fulltext" :provider "lucene"}]
-    (nodes/create-index name conf)))
+    (nodes/create-index name conf)
+    (nodes/delete-index name)))
 
 (deftest ^{:indexing true} test-create-a-new-unique-node-index-with-explicit-configuration
   (let [name "node-index-2b"
         conf {:unique true}]
-    (nodes/create-index name conf)))
+    (nodes/create-index name conf)
+    (nodes/delete-index name)))
 
-(deftest ^{:indexing true} test-create-a-new-rel-index-with-explicit-configuration
+(deftest ^{:indexing true :focus true} test-create-a-new-rel-index-with-explicit-configuration
   (let [name "rel-index-2"
         conf {:type "fulltext" :provider "lucene"}]
     (rels/create-index name conf)))
 
 (deftest ^{:indexing true} test-create-a-new-unique-rel-index-with-explicit-configuration
   (let [name "rel-index-2b"
-        conf {:unique true}]
+        conf {:unique true :provider "lucene" :type "fulltext"}]
     (rels/create-index name conf)))
 
 (deftest ^{:indexing true} test-listing-node-indexes
@@ -170,6 +172,10 @@
     (nodes/delete-from-index (:id home) (:name idx))))
 
 (deftest ^{:indexing true} test-removing-a-node-removes-it-from-indexes
+  (try
+    (nodes/create-index "by-url")
+    (catch Exception e
+      (comment We need to make sure we wipe previously created index with a different configuration, ignore cases when it simply does not exist)))
   (let [node1 (nodes/create {:name "Wired"})
         url1  "http://wired.com"
         idx   (nodes/create-index "by-url")]
@@ -180,26 +186,36 @@
       (is (not (ids (:id node1)))))))
 
 (deftest ^{:indexing true} test-finding-nodes-using-full-text-search-queries-over-index
+  (try
+    (nodes/delete-index "companies")
+    (catch Exception e
+      (comment We need to make sure we wipe previously created index with a different configuration, ignore cases when it simply does not exist)))
   (let [puma  (nodes/create {:name "Puma"  :hq-location "Herzogenaurach, Germany"})
         apple (nodes/create {:name "Apple" :hq-location "Cupertino, CA, USA"})
-        idx   (nodes/create-index "companies")]
+        idx   (nodes/create-index "companies" {:type :fulltext})]
     (nodes/delete-from-index (:id puma)  (:name idx))
     (nodes/delete-from-index (:id apple) (:name idx))
     (nodes/add-to-index (:id puma)  (:name idx) "country" "Germany")
     (nodes/add-to-index (:id apple) (:name idx) "country" "United States of America")
     (let [ids (set (map :id (nodes/query (:name idx) "country:Germany")))]
       (is (ids (:id puma)))
-      (is (not (ids (:id apple)))))))
+      (is (not (ids (:id apple)))))
+    (nodes/delete-index "companies")))
 
 
 (deftest ^{:indexing true} test-finding-nodes-using-full-text-search-queries-over-index-example2
+  (try
+    (nodes/delete-index "companies")
+    (catch Exception e
+      (comment We need to make sure we wipe previously created index with a different configuration, ignore cases when it simply does not exist)))         
   (let [neocons (nodes/create {:name "Neocons"  :description "Neocons is an idiomatic Clojure client for the Neo4J Server REST interface"})
         monger  (nodes/create {:name "Monger" :description "Monger is a Clojure MongoDB driver for a more civilized age"})
-        idx   (nodes/create-index "companies" {:type :fulltext :analyzer "org.apache.lucene.analysis.standard.StandardAnalyzer"})]
+        idx     (nodes/create-index "companies" {:type :fulltext :provider "lucene"})]
     (nodes/delete-from-index (:id neocons)  (:name idx))
     (nodes/delete-from-index (:id monger) (:name idx))
     (nodes/add-to-index (:id neocons) (:name idx) "description" "Neocons is an idiomatic Clojure client for the Neo4J Server REST interface")
     (nodes/add-to-index (:id monger)  (:name idx) "description" "Monger is a Clojure MongoDB driver for a more civilized age")
     (let [ids (set (map :id (nodes/query (:name idx) "description:*civilized*")))]
       (is (ids (:id monger)))
-      (is (not (ids (:id neocons)))))))
+      (is (not (ids (:id neocons)))))
+    (nodes/delete-index "companies")))
