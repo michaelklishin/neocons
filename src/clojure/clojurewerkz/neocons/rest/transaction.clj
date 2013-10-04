8113b677f8e5342a269c67ef954af85348594a5f
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2013-10-04 18:57:31 +0100

    Added some documentation

--- src/clojure/clojurewerkz/neocons/rest/transaction.clj
+++ src/clojure/clojurewerkz/neocons/rest/transaction.clj
@@ -24,6 +24,9 @@
   (map records/instantiate-cypher-query-response-from (:results payload)))
 
 (defn begin
+  "Starts a transaction with the given cypher statements and returns a transaction record along with
+  the result of the cypher statements. 0-arity creates a transaction without any cypher statements.
+  For more information, see http://docs.neo4j.org/chunked/milestone/rest-api-transactional.html#rest-api-begin-a-transaction"
   ([] (begin []))
   ([xs]
    (let [[status headers body]          (make-request xs (:transaction-uri rest/*endpoint*))
@@ -37,7 +40,13 @@
       [neo-trans (make-cypher-responses payload)]))))
 
 (defn execute
-  ([transaction] (execute transaction []))
+  "Executes cypher statements in an existing transaction and returns the new transaction record along
+  with the cypher results. If no cypher statement is give, the effect is to keep the transaction alive
+  (prevent it from timing out).
+
+  For more information, see http://docs.neo4j.org/chunked/milestone/rest-api-transactional.html#rest-api-execute-statements-in-an-open-transaction"
+
+  [transaction] (execute transaction []))
   ([transaction xs] (execute transaction xs (:location transaction)))
   ([transaction xs uri]
     (let [[status headers body]          (make-request xs uri)
@@ -51,12 +60,19 @@
          (make-cypher-responses payload)]))))
 
 (defn commit
+  "Commits an existing transaction with option cypher statements which are applied
+  before the transaction is committed.
+  For more information, see http://docs.neo4j.org/chunked/milestone/rest-api-transactional.html#rest-api-commit-an-open-transaction"
+
   ([transaction] (commit transaction []))
   ([transaction xs]
    (let [[_ result] (execute transaction xs (:commit transaction))]
     result)))
 
 (defn rollback
+  "Rolls back an existing transaction.
+  For more information, see http://docs.neo4j.org/chunked/milestone/rest-api-transactional.html#rest-api-rollback-an-open-transaction"
+
   [transaction]
   (let [{:keys [status headers body]} (rest/DELETE (:location transaction))]
     body))
