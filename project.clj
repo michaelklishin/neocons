82b23e94899f6bc6449edfc48511bbd22c94b7d5
Michael Klishin <michael@clojurewerkz.org>
2013-10-05 19:48:34 +0400

    Drop Clojure 1.3 support

--- project.clj
+++ project.clj
@@ -28,14 +28,13 @@
                                                (:indexing m)))
                    :all            (constantly true)}
   :source-paths ["src/clojure"]
-  :profiles       {:1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
-                   :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+  :profiles       {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
                    :1.6 {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
                    :master {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
                    :dev {:plugins [[codox "0.6.4"]]
                          :codox {:sources ["src/clojure"]
                                  :output-dir "doc/api"}}}
-  :aliases        {"all" ["with-profile" "dev:dev,1.3:dev,1.4:dev,1.6:dev,master"]}
+  :aliases        {"all" ["with-profile" "dev:dev,1.4:dev,1.6:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
