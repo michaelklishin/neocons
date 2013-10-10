dba4f5a7971c44cd0d943d16cff7d78014e47fa4
Michael Klishin <michael@clojurewerkz.org>
2013-10-10 21:39:20 +0400

    Reindent w/ clojure-mode

--- src/clojure/clojurewerkz/neocons/rest/labels.clj
+++ src/clojure/clojurewerkz/neocons/rest/labels.clj
@@ -14,31 +14,31 @@
   See http://docs.neo4j.org/chunked/milestone/rest-api-node-labels.html#rest-api-adding-a-label-to-a-node"
   [node labels]
   (rest/POST
-    (str (:location-uri node) "/labels")
-    :body (json/encode (conv/kw-to-string labels))))
+   (str (:location-uri node) "/labels")
+   :body (json/encode (conv/kw-to-string labels))))
 
 (defn replace
   "This removes any existing labels for the node and adds the labels passes to the function.
   See http://docs.neo4j.org/chunked/milestone/rest-api-node-labels.html#rest-api-replacing-labels-on-a-node"
   [node labels]
   (conv/string-to-kw
-    (rest/PUT
-      (str (:location-uri node) "/labels")
-      :body (json/encode labels))))
+   (rest/PUT
+    (str (:location-uri node) "/labels")
+    :body (json/encode labels))))
 
 (defn remove
   "This removes the specified label from the node.
   See http://docs.neo4j.org/chunked/milestone/rest-api-node-labels.html#rest-api-removing-a-label-from-a-node"
   [node label]
   (rest/DELETE
-    (str (:location-uri node) "/labels/" (conv/kw-to-string label))))
+   (str (:location-uri node) "/labels/" (conv/kw-to-string label))))
 
 (defn- get-labels
   [^String uri]
   (let [{:keys [status headers body]} (rest/GET uri)]
     (when-not (missing? status)
       (conv/string-to-kw
-        (json/decode body true)))))
+       (json/decode body true)))))
 
 (defn get-all-labels
   "This function gets all labels in the database if no argument is passed.
@@ -58,7 +58,7 @@
        (when (and x y)
          (str "?"
               (http/generate-query-string
-                [[(conv/kw-to-string x) (json/encode y)]])))))
+               [[(conv/kw-to-string x) (json/encode y)]])))))
 
 
 (defn get-all-nodes
@@ -69,7 +69,7 @@
   See http://docs.neo4j.org/chunked/milestone/rest-api-node-labels.html#rest-api-get-nodes-by-label-and-property"
   ([label] (get-all-nodes label nil nil))
   ([label prop-name prop-value]
-   (let [base-uri (encode-params label prop-name prop-value)
-         {:keys [status headers body]} (rest/GET base-uri)]
-     (when-not (missing? status)
-       (map records/instantiate-node-from (json/decode body true))))))
+     (let [base-uri (encode-params label prop-name prop-value)
+           {:keys [status headers body]} (rest/GET base-uri)]
+       (when-not (missing? status)
+         (map records/instantiate-node-from (json/decode body true))))))
