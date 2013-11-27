aa53963f20100236aed6dc2c0305ff1b3144e19a
Michael Klishin <michael@clojurewerkz.org>
2013-11-27 23:30:21 +0400

    Don't use :use

--- test/clojurewerkz/neocons/rest/test/basic_http_authentication_test.clj
+++ test/clojurewerkz/neocons/rest/test/basic_http_authentication_test.clj
@@ -1,9 +1,9 @@
 (ns clojurewerkz.neocons.rest.test.basic-http-authentication-test
   (:require [clojurewerkz.neocons.rest :as neorest]
             [clojurewerkz.neocons.rest.nodes :as nodes]
-            [slingshot.slingshot :as slingshot])
-  (:import [slingshot ExceptionInfo])
-  (:use clojure.test))
+            [slingshot.slingshot :as slingshot]
+            [clojure.test :refer :all])
+  (:import [slingshot ExceptionInfo]))
 
 ;; This group of tests assumes you have Nginx or Apache proxy set up at neo4j-proxy.local
 ;; that proxies to whatever Neo4J Server installation you want to use. It is excluded from default
