8d86050abe6658b28a8bdf657f35a93ecc0fb951
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2014-01-08 09:33:58 +0000

    Wrapping tests in a is statement and consequently fixes broken tests. Addresses #45

--- test/clojurewerkz/neocons/rest/test/transaction_test.clj
+++ test/clojurewerkz/neocons/rest/test/transaction_test.clj
@@ -26,74 +26,74 @@
          :location
          :expires)
     (is (= [] y))
-    (= (tx/rollback transaction) [])))
+    (is (= (tx/rollback transaction) []))))
 
 (deftest ^{:edge-features true} test-transaction-rollback
-  (let [[transaction result] (tx/begin [(tx/statement "CREATE (n {props}) RETURN n" {:props {:name "My Node"}})])]
+  (let [[transaction [result]] (tx/begin [(tx/statement "CREATE (n {props}) RETURN n" {:props {:name "My Node"}})])]
     (are [x] (not (nil? (x transaction)))
          :commit
          :location
          :expires)
 
-    (= (:data result) [{:row [{:name "My Node"}]}])
-    (= (:columns result) ["n"])
-    (= (tx/rollback transaction) [])))
+    (is (= (:data result) [{:row [{:name "My Node"}]}]))
+    (is (= (:columns result) ["n"]))
+    (is (= (tx/rollback transaction) []))))
 
 (deftest ^{:edge-features true} test-transaction-commit-empty
-  (let [[transaction result] (tx/begin [(tx/statement "CREATE (n {props}) RETURN n" {:props {:name "My Node"}})])]
+  (let [[transaction [result]] (tx/begin [(tx/statement "CREATE (n {props}) RETURN n" {:props {:name "My Node"}})])]
     (are [x] (not (nil? (x transaction)))
          :commit
          :location
          :expires)
 
-    (= (:data result) [{:row [{:name "My Node"}]}])
-    (= (:columns result) ["n"])
+    (is (= (:data result) [{:row [{:name "My Node"}]}]))
+    (is (= (:columns result) ["n"]))
 
-    (= (tx/commit transaction) [])))
+    (is (= (tx/commit transaction) []))))
 
 (deftest ^{:edge-features true} test-transaction-commit
-  (let [[transaction result] (tx/begin [(tx/statement "CREATE (n {props}) RETURN n" {:props {:name "My Node"}})])]
+  (let [[transaction [result]] (tx/begin [(tx/statement "CREATE (n {props}) RETURN n" {:props {:name "My Node"}})])]
     (are [x] (not (nil? (x transaction)))
          :commit
          :location
          :expires)
 
-    (= (:data result) [{:row [{:name "My Node"}]}])
-    (= (:columns result) ["n"])
+    (is (= (:data result) [{:row [{:name "My Node"}]}]))
+    (is (= (:columns result) ["n"]))
 
-    (let [result (tx/commit transaction [(tx/statement "CREATE n RETURN id(n)" nil)] )]
-      (= (:columns result) ["id(n)"])
-      (= (count (:data result)) 1))))
+    (let [[result] (tx/commit transaction [(tx/statement "CREATE n RETURN id(n)" nil)] )]
+      (is (= (:columns result) ["id(n)"]))
+      (is (= (count (:data result)) 1)))))
 
 (deftest ^{:edge-features true} test-transaction-continue-commit
-  (let [[transaction result] (tx/begin [(tx/statement "CREATE (n {props}) RETURN n" {:props {:name "My Node"}})])]
+  (let [[transaction [result]] (tx/begin [(tx/statement "CREATE (n {props}) RETURN n" {:props {:name "My Node"}})])]
     (are [x] (not (nil? (x transaction)))
          :commit
          :location
          :expires)
 
-    (= (:data result) [{:row [{:name "My Node"}]}])
-    (= (:columns result) ["n"])
+    (is (= (:data result) [{:row [{:name "My Node"}]}]))
+    (is (= (:columns result) ["n"]))
 
-    (let [[a b] (tx/execute transaction [(tx/statement "CREATE n RETURN id(n)" nil)] )]
+    (let [[a [b]] (tx/execute transaction [(tx/statement "CREATE n RETURN id(n)" nil)] )]
       (are [x] (not (nil? (x a)))
            :commit
            :location
            :expires)
-      (= (count (:data b)) 1)
-      (= (:columns b) ["id(n)"]))
+      (is (= (count (:data b)) 1))
+      (is (= (:columns b) ["id(n)"])))
 
-    (= (tx/commit transaction) [])))
+    (is (= (tx/commit transaction) []))))
 
 (deftest ^{:edge-features true} test-transaction-fail-rollback
-  (let [[transaction result] (tx/begin [(tx/statement "CREATE (n {props}) RETURN n" {:props {:name "My Node"}})])]
+  (let [[transaction [result]] (tx/begin [(tx/statement "CREATE (n {props}) RETURN n" {:props {:name "My Node"}})])]
     (are [x] (not (nil? (x transaction)))
          :commit
          :location
          :expires)
 
-    (= (:data result) [{:row [{:name "My Node"}]}])
-    (= (:columns result) ["n"])
+    (is (= (:data result) [{:row [{:name "My Node"}]}]))
+    (is (= (:columns result) ["n"]))
 
     (is (thrown-with-msg? Exception #"Transaction failed and rolled back"
                           (tx/execute
@@ -123,13 +123,13 @@
     (is (= (tx/with-transaction
              transaction
              true
-             (let [[_ r] (tx/execute
+             (let [[_ [r]] (tx/execute
                            transaction
                            [(tx/statement "CREATE (n {props}) RETURN n"
                                           {:props {:name "My Node"}})])]
-               (= (count (:data r)) 1)
-               (= (:data r) [{:row [{:name "My Node"}]}])
-               (= (:columns r) ["n"]))))
+               (is (= (count (:data r)) 1))
+               (is (= (:data r) [{:row [{:name "My Node"}]}]))
+               (is (= (:columns r) ["n"])))))
         [])))
 
 
@@ -138,13 +138,13 @@
     (tx/with-transaction
       transaction
       false
-      (let [[_ r] (tx/execute
+      (let [[_ [r]] (tx/execute
                     transaction
                     [(tx/statement "CREATE (n {props}) RETURN n"
                                    {:props {:name "My Node"}})])]
-        (= (count (:data r)) 1)
-        (= (:data r) [{:row [{:name "My Node"}]}])
-        (= (:columns r) ["n"]))
+        (is (= (count (:data r)) 1))
+        (is (= (:data r) [{:row [{:name "My Node"}]}]))
+        (is (= (:columns r) ["n"])))
       (is (= (tx/rollback transaction)
              [])))))
 
@@ -162,7 +162,7 @@
 (deftest ^{:edge-features true} test-with-transaction-transaction-failure
   (let [transaction (tx/begin-tx)]
     (is (thrown-with-msg?
-          Exception #"STATEMENT_SYNTAX_ERROR"
+          Exception #"InvalidSyntax"
           (tx/with-transaction
             transaction
             true
