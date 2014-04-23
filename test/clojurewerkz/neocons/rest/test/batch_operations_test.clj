97c2015d473841fb647a68828e2199d1f626647c
Michael Klishin <michael@clojurewerkz.org>
2014-04-23 23:25:55 +0400

    Avoid using a dynamic var for connection in tests
    That goes against the grain of the new API.

--- test/clojurewerkz/neocons/rest/test/batch_operations_test.clj
+++ test/clojurewerkz/neocons/rest/test/batch_operations_test.clj
@@ -12,38 +12,35 @@
   (:require [clojurewerkz.neocons.rest               :as neorest]
             [clojurewerkz.neocons.rest.nodes         :as nn]
             [clojurewerkz.neocons.rest.batch         :as b]
-            [clojure.test :refer :all]
-            [clojurewerkz.neocons.rest.test.common   :refer :all]))
+            [clojure.test :refer :all]))
 
-(use-fixtures :once once-fixture)
-
-
-(deftest ^{:batching true} test-basic-batching-of-inserts
-         (let [n     100
-               rng   (range 0 n)
-               xs    (doall (map (fn [x] {:n x}) rng))
-               nodes (doall (nn/create-batch *connection* xs))]
-           ;; ensure that we aren't tripped by laziness
-           (is (= (count nodes) n))
-           (is (= (count (map :id nodes)) n))
-           (nn/delete-many *connection* (vec nodes))))
+(let [conn (neorest/connect "http://localhost:7474/db/data/")]
+  (deftest ^{:batching true} test-basic-batching-of-inserts
+    (let [n     100
+          rng   (range 0 n)
+          xs    (doall (map (fn [x] {:n x}) rng))
+          nodes (doall (nn/create-batch conn xs))]
+      ;; ensure that we aren't tripped by laziness
+      (is (= (count nodes) n))
+      (is (= (count (map :id nodes)) n))
+      (nn/delete-many conn (vec nodes))))
 
 
 
-(deftest ^{:batching true} test-generic-batch-operations-support
-         (let [ops [{:method "POST"
-                     :to     "/node"
-                     :body   {}
-                     :id     0}
-                    {:method "POST"
-                     :to     "/node"
-                     :body   {}
-                     :id     1}
-                    {:method "POST",
-                     :to     "{0}/relationships",
-                     :body   {:to   "{1}"
-                              :data {}
-                              :type "knows"}
-                     :id     2}]
-               res (doall (b/perform *connection* ops))]
-           (is (= (count res) (count ops)))))
+  (deftest ^{:batching true} test-generic-batch-operations-support
+    (let [ops [{:method "POST"
+                :to     "/node"
+                :body   {}
+                :id     0}
+               {:method "POST"
+                :to     "/node"
+                :body   {}
+                :id     1}
+               {:method "POST",
+                :to     "{0}/relationships",
+                :body   {:to   "{1}"
+                         :data {}
+                         :type "knows"}
+                :id     2}]
+          res (doall (b/perform conn ops))]
+      (is (= (count res) (count ops))))))
