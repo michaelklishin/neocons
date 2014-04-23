998732419f3c747137dba07a122f1f10a7e4038a
Michael Klishin <michael@clojurewerkz.org>
2014-04-23 23:26:43 +0400

    Remove a namespace that's no longer used

--- test/clojurewerkz/neocons/rest/test/common.clj
+++
@@ -1,9 +0,0 @@
-(ns clojurewerkz.neocons.rest.test.common
-  (:require [clojurewerkz.neocons.rest :as neorest]))
-
-(def ^{:dynamic true} *connection*)
-
-(defn once-fixture
-  [f]
-  (binding [*connection*  (neorest/connect "http://localhost:7474/db/data/")]
-    (f)))
