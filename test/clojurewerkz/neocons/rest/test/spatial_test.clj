3cd73001ff7f2f23a6ddcba16e9459f046f46eb3
Michael Klishin <michael@clojurewerkz.org>
2014-04-23 23:26:30 +0400

    Ditto

--- test/clojurewerkz/neocons/rest/test/spatial_test.clj
+++ test/clojurewerkz/neocons/rest/test/spatial_test.clj
@@ -12,27 +12,26 @@
   (:require [clojurewerkz.neocons.rest               :as neorest]
             [clojurewerkz.neocons.rest.nodes         :as nodes]
             [clojurewerkz.neocons.rest.spatial       :as sp]
-            [clojurewerkz.neocons.rest.test.common   :refer :all]
             [clojure.test :refer :all]))
 
-(use-fixtures :once once-fixture)
-
-(deftest ^{:spatial true} test-create-layer
-  (let [created-layer-1 (sp/add-simple-point-layer *connection* "test-layer-1")
-        created-layer-2 (sp/add-simple-point-layer *connection* "test-layer-2" "x" "y")]
+(let [conn (neorest/connect "http://localhost:7474/db/data/")]
+  (deftest ^{:spatial true} test-create-layer
+  (let [created-layer-1 (sp/add-simple-point-layer conn "test-layer-1")
+        created-layer-2 (sp/add-simple-point-layer conn "test-layer-2" "x" "y")]
     (is (= "test-layer-1" (:layer (:data created-layer-1))))
     (is (= "test-layer-2" (:layer (:data created-layer-2))))
     (is (= "y:x" (:geomencoder_config (:data created-layer-2))))))
 
 (deftest ^{:spatial true} test-add-node-to-layer
-  (let [created-layer-1 (sp/add-simple-point-layer *connection* "test-layer-add")
-        test-node (nodes/create *connection* {:latitude 60.1 :longitude 15.4 :foo "bar"})
-        added-node (sp/add-node-to-layer *connection* "test-layer-add" test-node)]
+  (let [created-layer-1 (sp/add-simple-point-layer conn "test-layer-add")
+        test-node (nodes/create conn {:latitude 60.1 :longitude 15.4 :foo "bar"})
+        added-node (sp/add-node-to-layer conn "test-layer-add" test-node)]
     (is (= [15.4 60.1 15.4 60.1] (:bbox (:data added-node))))))
 
 (deftest ^{:spatial true} test-find-within-distance
-  (let [created-layer-1 (sp/add-simple-point-layer *connection* "test-layer-search")
-        test-node (nodes/create *connection* {:latitude -60.1 :longitude -15.4 :foo "target test node"})
-        added-node (sp/add-node-to-layer *connection* "test-layer-search" test-node)
-        found (sp/find-within-distance *connection* "test-layer-search" -15.0 -60.0 100)]
-    (is (= "target test node" (:foo (:data (first found)))))))
+  (let [created-layer-1 (sp/add-simple-point-layer conn "test-layer-search")
+        test-node (nodes/create conn {:latitude -60.1 :longitude -15.4 :foo "target test node"})
+        added-node (sp/add-node-to-layer conn "test-layer-search" test-node)
+        found (sp/find-within-distance conn "test-layer-search" -15.0 -60.0 100)]
+    (is (= "target test node" (:foo (:data (first found))))))))
+
