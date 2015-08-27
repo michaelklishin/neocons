3edc7a6bfb3b84a396bb85ed27d4a1f003792eca
Michael Klishin <michael@clojurewerkz.org>
2015-08-27 03:06:07 +0300

    3.1.0-rc1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/neocons "3.1.0-beta4-SNAPSHOT"
+(defproject clojurewerkz/neocons "3.1.0-rc1"
   :description "Neocons is a feature rich idiomatic Clojure client for the Neo4J REST API"
   :url "http://clojureneo4j.info"
   :license {:name "Eclipse Public License"}
