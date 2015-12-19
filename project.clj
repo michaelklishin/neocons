e204d72778fc27d6c193a7063ccf32f002cb9ff5
Michael Klishin <michael@clojurewerkz.org>
2015-12-20 02:15:20 +0300

    Test against Clojure 1.8 (RC4)

--- project.clj
+++ project.clj
@@ -34,6 +34,7 @@
                    :all            (constantly true)}
   :source-paths ["src/clojure"]
   :profiles       {:1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+                   :1.8 {:dependencies [[org.clojure/clojure "1.8.0-RC4"]]}
                    :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}
                    :dev {:plugins [[lein-codox "0.9.0"]]
                          :codox {:source-paths ["src/clojure"]}}
@@ -43,7 +44,7 @@
                    :cljhttp076 {:dependencies [[clj-http "0.7.6"]]}}
   :codox {:src-dir-uri "https://github.com/michaelklishin/neocons/blob/master/"
           :src-linenum-anchor-prefix "L"}
-  :aliases        {"all" ["with-profile" "dev:dev,1.6:dev,master:dev,cljhttp076:dev,1.6,cljhttp076"]}
+  :aliases        {"all" ["with-profile" "dev:dev,1.6:dev,1.8:dev,master:dev,cljhttp076:dev,1.6,cljhttp076"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
