8e4bd10ca7ed86c17b57aa58a09888376acef664
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2016-07-29 14:17:19 +0100

    Upgraded to latest Neo4j version

--- bin/ci/start_neo4j_server.sh
+++ bin/ci/start_neo4j_server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-VERSION="2.2.5"
-TARBALL="neo4j2.2.tar.gz"
+VERSION="3.0.3"
+TARBALL="neo4j3.0.tar.gz"
 
 cd /tmp
 wget -O $TARBALL "http://dist.neo4j.org/neo4j-community-$VERSION-unix.tar.gz?edition=community&version=$VERSION&distribution=tarball&dlid=2803678"
@@ -10,4 +10,4 @@ tar zxf $TARBALL
 cd "neo4j-community-$VERSION"
 
 ./bin/neo4j start
-sleep 3
+sleep 5
