d5902208cda62bec6062ca6b2b0a5bcd63f8a19b
Michael Klishin <michael@clojurewerkz.org>
2018-01-18 00:45:27 +0300

    Pass ShellCheck; provision Neo4J Server 3.3.1

--- bin/ci/start_neo4j_server.sh
+++ bin/ci/start_neo4j_server.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-VERSION="3.0.3"
-TARBALL="neo4j3.0.tar.gz"
+VERSION="3.3.1"
+TARBALL="neo4j-server-$VERSION.tar.gz"
 
-cd /tmp
+cd /tmp || exit
 wget -O $TARBALL "http://dist.neo4j.org/neo4j-community-$VERSION-unix.tar.gz?edition=community&version=$VERSION&distribution=tarball&dlid=2803678"
 tar zxf $TARBALL
 
-cd "neo4j-community-$VERSION"
+cd "neo4j-community-$VERSION" || exit
 
 ./bin/neo4j start
 sleep 5
