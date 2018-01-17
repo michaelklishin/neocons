a6e2d753fa19440802fa9e8e80d674048b7fa705
Michael Klishin <michael@clojurewerkz.org>
2018-01-18 00:45:27 +0300

    Pass ShellCheck; provision Neo4J Server 3.3.1
    (cherry picked from commit d5902208cda62bec6062ca6b2b0a5bcd63f8a19b)

--- bin/ci/test.sh
+++ bin/ci/test.sh
@@ -2,7 +2,7 @@
 
 lein run -m clojurewerkz.neocons.rest.passwords http://localhost:7474/ neo4j neo4j qwerty
 
-if ["$TRAVIS_SECURE_ENV_VARS" = "true"]; then
+if test "$TRAVIS_SECURE_ENV_VARS" = 'true'; then
   NEO4J_LOGIN=neo4j NEO4J_PASSWORD=qwerty lein test :travis
 else
   NEO4J_LOGIN=neo4j NEO4J_PASSWORD=qwerty lein test :default
